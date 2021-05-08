3262e368f784f62293a292ed1ea191c2a7ea3c52
ZitronCrazy <zitro5@protonmail.com>
2021-05-08 20:14:00 +0200

    DayOfTheWeek

---
+++ src/DayOfTheWeek.java
@@ -0,0 +1,49 @@
+public class DayOfTheWeek {
+
+    public static void printDayOfTheWeek(int day) {
+
+        switch (day) {
+            case 0:
+                System.out.println("It's Sunday");
+                break;
+            case 1:
+                System.out.println("It's Monday");
+                break;
+            case 2:
+                System.out.println("It's Tuesday");
+                break;
+            case 3:
+                System.out.println("It's Wednesday");
+                break;
+            case 4:
+                System.out.println("It's Thursday");
+                break;
+            case 5:
+                System.out.println("It's Friday");
+                break;
+            case 6:
+                System.out.println("It's Saturday");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+
+            //Other method:
+            // if(day == 0) {
+            //      System.out.println("It's Sunday");
+            //} else if(day == 1) {
+            //     System.out.println("It's Monday")
+            //} else if(day == 2) {
+            //     System.out.println("It's Tuesday")
+            //} else if(day == 3) {
+            //     System.out.println("It's Wednesday")
+            //} else if(day == 4) {
+            //     System.out.println("It's Thursday")
+            //} else if(day == 5) {
+            //     System.out.println("It's Friday")
+            //} else if(day == 6) {
+            //     System.out.println("It's Saturday")
+            //}
+        }
+    }
+}
